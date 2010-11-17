535c545d3fc3ee604161c10bc3dad562cf6eff65
Gerd Van Zegbroeck <gerd@vanzegbroeck.net>
2010-11-18 00:12:28 +0100

    adding licence info

--- Sources/Main.m
+++ Sources/Main.m
@@ -1,14 +1,40 @@
+//	--------------------------------------------------------------------------------------------------------------------
 //
-//  main.m
+//  Main.h
 //  APNSTest
 //
 //  Created by Gerd Van Zegbroeck on 16/11/10.
-//  Copyright 2010 Managing Software. All rights reserved.
 //
+//  Managing Software : http://www.managingsoftware.com
+//
+//	--------------------------------------------------------------------------------------------------------------------
+//
+//	This file is part of APNSTest - Apple Push Notification Test.
+//
+//	APNSTest is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	APNSTest is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with APNSTest. If not, see <http://www.gnu.org/licenses/>.
+//
+//	--------------------------------------------------------------------------------------------------------------------
 
 #import <Cocoa/Cocoa.h>
+
+//	--------------------------------------------------------------------------------------------------------------------
+//	function main
+//	--------------------------------------------------------------------------------------------------------------------
 
 int main(int argc, char *argv[])
 {
     return NSApplicationMain(argc,  (const char **) argv);
 }
+
+//	--------------------------------------------------------------------------------------------------------------------
